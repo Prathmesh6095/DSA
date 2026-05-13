985e9aceae2c1ae8881b4bf1390333630713891a
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-05-13 09:26:33 +0530

    Functions in java

--- Main.java
+++ Main.java
@@ -1,7 +1,16 @@
 package DSA;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+class Main {
+    public static int patu (int n)
+    {
+       int res = n*2;
+       return res;
     }
-}
+    public static void main(String[] args) {
+        
+       int n=10;
+       int res = patu(n);
+       
+       System.out.println(res);
+    }
+    }
