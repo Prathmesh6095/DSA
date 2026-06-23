020852bb5718a6f269a3f7ede418d158828c31da
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-06-23 20:07:46 +0530

    box pattern in java

---
+++ Patterns/Box.java
@@ -0,0 +1,23 @@
+package Patterns;
+
+public class Box {
+    public static void main(String[] args) {
+        int n = 4;
+        int m = 5;
+
+        //outer loop
+        for(int i=1; i<=n; i++){
+            //inner loop
+            for(int j=1; j<=5; j++) {
+                if (i==1 || j==1 || i==n || j==m) {
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+}
