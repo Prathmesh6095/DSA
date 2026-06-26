c122501a7c86189784c9ef84f71958fe95acadfb
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-06-26 11:17:58 +0530

    Floyd's 0-1 Triangle

--- Patterns/Triangle01.java
+++ Patterns/Triangle01.java
@@ -5,7 +5,7 @@ public class Triangle01 {
         int n=5;
 
         for(int i=1;i<=n;i++){
-            for(int j=1;j <=i;j++){
+            for(int j=1;j<=i;j++){
                 int sum = i+j;
                 if (sum % 2 ==0) {
                     System.out.print("1 ");
