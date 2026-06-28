e0f2beece3aa06cf017be895ca85e5ff42476826
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-06-28 10:29:19 +0530

    Arrays in java with user input

--- Array/Arrays.java
+++ Array/Arrays.java
@@ -1,14 +1,22 @@
 package Array;
+
 import java.util.*;
+
 public class Arrays {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
         int numbers[] = new int[size];
 
-        for(int i=0;i<size;i++){
+        // // input
+        for(int i=0; i<size; i++){
+            numbers[i] = sc.nextInt();
+        }
+
+        // output
+        for (int i = 0; i < size; i++) {
             System.out.println(numbers[i]);
         }
-        
+
     }
 }
